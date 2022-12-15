19c096a0c6d775b4973bb8fe9cf830e5b0ea95d8
school-37 <good6857632@gmail.com>
2022-12-15 19:11:54 +0800

    crawler3練習

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -4,48 +4,25 @@
 // 1. 安裝 npm i axios
 // 2. 引用 require
 // 3. 去讀官方文件
-const axios = require('axios');
-
+const axios = require("axios");
+const fs = require("fs/promises");
+// const moment = require("moment");
 // http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
 
-const fs = require('fs');
-
-let p = (fileName) => {
-    let toreadFile = new Promise((reject, resolve) => {
-       fs.readFile(fileName, "utf-8", (err, data) => {
-        if(err) {
-            resolve(err);
-        }else{
-            reject(`成功讀到資料:, ${data}`);
-        }
-       });
-    });
-    return toreadFile;
-};
-p('test.txt').then((data) => {
-    console.log(data)
-}).catch((err) => {
-    console.log(err)
-});
-p('test-1.txt').then((data) => {
-    console.log(data)
-}).catch((err) => {
-    console.log(err)
-});
-
 (async () => {
-    try {
-      let stockNo = '2618';
-      let date = '20221111';
-      let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
-        params: {
-          stockNo,
-          date,
-        },
-      });
-  
-      console.log('await', response.data);
-    } catch (e) {
-      console.error(e);
-    }
-  })();
+  try {
+    let stockNo = await fs.readFile("stock.txt", "utf-8");
+    let date = '20221111'
+    // moment().format('YYYYMMDD')
+    let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
+      params: {
+        stockNo,
+        date,
+      },
+    });
+
+    console.log("await", response.data);
+  } catch (e) {
+    console.error(e);
+  }
+})();
