36f2e1fe40d757cd986390fbafe781a5b061df46
school-37 <good6857632@gmail.com>
2022-12-17 16:29:49 +0800

    更新express

--- stock-be/server.js
+++ stock-be/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 // 利用 express 這個框架建立一個 web app
 const app = express();
 
+//中間件
+app.use((req, res, next) => {
+    console.log('這是一個中間件');
+    next();
+})
+
 // app.[Method]
 // get, post, put, patch, delete, option, head
 app.get('/', (req, res) => {
-  res.send('Hello Express 5');
+  res.send('Hello Express 2');
 });
 
 app.listen(3001, () => {
