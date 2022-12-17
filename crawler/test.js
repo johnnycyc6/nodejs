392633f305c92066250ecce12fb41987f23ada97
school-37 <good6857632@gmail.com>
2022-12-17 12:08:36 +0800

    新增env

--- crawler/test.js
+++ crawler/test.js
@@ -1,13 +1,16 @@
 const mysql2 = require('mysql2/promise');
+require('dotenv').config();
 
 (async () => {
     const connection = await mysql2.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'admin',
-        password: '',
-        database: 'stock_mfee31',
+        
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
     });
+    
     // simple query
   let result = await connection.query('SELECT * FROM `stocks`');
   let data = result[0];
